26eb7da48426c4381765265f1d07eb4d9f8941d2
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-07-06 18:18:12 +0530

    rename assets output dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rokalin.co.uk',
+  build: {
+    assets: 'astro_',
+  },
   integrations: [tailwind()]
 });
