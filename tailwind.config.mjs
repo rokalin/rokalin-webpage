698b8ba9a7ed90fdaf2ffbf3f726482f2a8dce58
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-07-28 17:19:18 +0530

    new navbar using company colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rokalin-teal': {
+					300: '#b1ccc7',
+					400: '#94bbb4',
+					500: '#86b0a9',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
